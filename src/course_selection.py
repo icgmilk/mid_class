8d8c7e57ffd0a07d6e265f503491ea2436e6a56b
AW-AlanWu <alan.92925@gmail.com>
2024-11-16 00:10:16 +0800

    Feat: implement show student course function

--- src/course_selection.py
+++ src/course_selection.py
@@ -42,16 +42,16 @@ if not students:
 
 if not courses:
     courses = {
-        "001419": {"name": "作業系統", "credits": 3, "schedule": {1 : [3, 4], 2 : [6]}},
-        "001432": {"name": "軟體工程開發實務", "credits": 3, "schedule": {3 : [6, 7, 8]}},
-        "001434": {"name": "資料科學實務", "credits": 3, "schedule": {2 : [3, 4], 3 : [2]}},
-        "001433": {"name": "資料探勘導論", "credits": 3, "schedule": {4 : [2, 3, 4]}},
-        "001439": {"name": "編譯器", "credits": 3, "schedule": {1 : [6], 3 : [3, 4]}},
-        "114514": {"name": "體育 - 空手道", "credits": 3, "schedule": {5 : [6, 7, 8]}},
-        "150449": {"name": "化妝品科學與應用", "credits": 3 , "schedule": {3 : [7, 8, 9]}},
-        "228922": {"name": "生命科學", "credits": 3 , "schedule": {2 : [7, 8, 9]}},
-        "383403": {"name": "微積分(一)", "credits": 4, "schedule": {1 : [1, 2], 4 : [6, 7]}},
-        "065535": {"name": "普通物理(二)", "credits": 3, "schedule": {5 : [1, 2, 3]}}
+        "001419": {"name": "作業系統", "credits": 3, "schedule": {'1' : [3, 4], '2' : [6]}},
+        "001432": {"name": "軟體工程開發實務", "credits": 3, "schedule": {'3' : [6, 7, 8]}},
+        "001434": {"name": "資料科學實務", "credits": 3, "schedule": {'2' : [3, 4], '3' : [2]}},
+        "001433": {"name": "資料探勘導論", "credits": 3, "schedule": {'4' : [2, 3, 4]}},
+        "001439": {"name": "編譯器", "credits": 3, "schedule": {'1' : [6], '3' : [3, 4]}},
+        "114514": {"name": "體育 - 空手道", "credits": 3, "schedule": {'5' : [6, 7, 8]}},
+        "150449": {"name": "化妝品科學與應用", "credits": 3 , "schedule": {'3' : [7, 8, 9]}},
+        "228922": {"name": "生命科學", "credits": 3 , "schedule": {'2' : [7, 8, 9]}},
+        "383403": {"name": "微積分(一)", "credits": 4, "schedule": {'1' : [1, 2], '4' : [6, 7]}},
+        "065535": {"name": "普通物理(二)", "credits": 3, "schedule": {'5' : [1, 2, 3]}}
     }
 
 # Utility Functions
@@ -61,16 +61,26 @@ def authenticate_student(student_id):
     return False
 
 def show_courses():
-    global date
     for(course_id, course) in courses.items():
         print(f"{course_id} - {course['name']} ({course['credits']} credits)")
         print("Schedule:")
         for day, periods in course['schedule'].items():
-            print(f"{date[int(day)]}: {', '.join([str(period) for period in periods])}")
+            print(f"{DATE[int(day)]}: {', '.join([str(period) for period in periods])}")
         print()
 
 def show_student_courses(student_id):
-    pass
+    course_ids = students[student_id]['courses']
+    timetable = [dict() for _ in range(8)]
+    for course_id in course_ids:
+        for day, periods in courses[course_id]['schedule'].items():
+            for time in periods:
+                timetable[int(day)][time] = courses[course_id]['name']
+    
+    for day, schedule in enumerate(timetable[1:], start=1):
+        print(f"{DATE[day]}:", end=' ')
+        for time, name in schedule.items():
+            print(f"|{time} {name}|", end=' ')
+        print()
 
 def add_course(student_id, course_id):
     # This check does not describe in system requirement
@@ -97,15 +107,15 @@ def add_course(student_id, course_id):
         return False
     
     for enrolled_course_id in student['courses']:
-        for day, period in courses[enrolled_course_id]['schedule'].items():
-            if day in course['schedule'] and list_intersect(course['schedule'][day], period):
+        for day, periods in courses[enrolled_course_id]['schedule'].items():
+            if day in course['schedule'] and list_intersect(course['schedule'][day], periods):
                 print("\nError: Course schedule conflicts with an already enrolled course.\n")
                 return
     
     student['courses'].append(course_id)
     student['credits'] += course['credits']
 
-    print(f"Add Course { course['name'] }({ course_id }) Successful!")
+    print(f"\nAdd Course { course['name'] }({ course_id }) Successful!\n")
 
 def drop_course(student_id, course_id):
     pass
@@ -129,7 +139,7 @@ def main():
         if choice == '1':
             show_courses()
         elif choice == '2':
-            pass
+            show_student_courses(student_id)
         elif choice == '3':
             course_id = input("Please enter course ID you want to add: ")
             add_course(student_id, course_id)
